a71c39ee879c44e68905c434ef5d393ee0cb5f90
Milana <70140326+Milana-miko@users.noreply.github.com>
2023-05-01 17:24:16 +0400

    Homework 9

--- hw9.js
+++ hw9.js
@@ -5,10 +5,10 @@ const str = "Lorem Ipsum is simply dummy text of";
 function allWordsUpperCase(str1) {
     const arr = str1.split(" ");
     for(let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1);
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1);
     }
+
     return arr.join(" ");
-    
 }
 
 console.log(allWordsUpperCase(str));
@@ -16,10 +16,10 @@ console.log(allWordsUpperCase(str));
 //Task 2  Ani to Ina
 
 function reversedWord(name) {
-    let word = name.charAt(0).toLowerCase() + name.substring(1, name.length-1) + name.charAt(name.length-1).toUpperCase();
+    const word = name.charAt(0).toLowerCase() + name.substring(1, name.length-1) + name.charAt(name.length-1).toUpperCase();
     return word.split('').reverse().join('');
 } 
 
 console.log(reversedWord("Ani"));
-
-
+console.log(reversedWord("Milana"));
+console.log(reversedWord("Anahit"));
