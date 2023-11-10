c438594a674f47f661cbda3467f42c23304da403
Milana <milanamiko88@gmail.com>
2023-11-10 23:11:18 +0400

    Change comment

--- hw8.js
+++ hw8.js
@@ -1,4 +1,4 @@
-
+//Test
 function convertNumberToWords (num) {
     const numberToWord = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
     return numberToWord[num];
